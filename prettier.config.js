01f10674935bbeb13f02eebb94dd82a487ba2679
Grudinskiy <aleksandr.grudinskiy@compliance.ai>
2023-06-12 22:13:59 +0300

    Initial commit

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  singleQuote: true,
+  trailingComma: "all",
+  // eslint-disable-next-line no-undef
+  plugins: [require('prettier-plugin-tailwindcss')],
+  tailwindConfig: './tailwind.config.js',
+}
---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import * as path from 'path';
+
+import react from '@vitejs/plugin-react';
+import checker from 'vite-plugin-checker';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    checker({
+      typescript: true,
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
