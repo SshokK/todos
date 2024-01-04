03417dffc45f05712b57e69c3b03e6c717877ed6
Grudinskiy <aleksandr.grudinskiy@compliance.ai>
2024-01-04 23:02:01 +0300

    feat: Add translation

--- src/views/App/elements/AppNavbar/elements/TodosCounts/TodosCounts.constants.ts
+++ src/views/App/elements/AppNavbar/elements/TodosCounts/TodosCounts.constants.ts
@@ -13,23 +13,27 @@ export const TODOS_COUNT_CONFIG: Record<
   TODOS_COUNT_TYPE,
   {
     Icon: ComponentType<Record<string, unknown>>;
-    tooltipTitle: string;
+    tooltipTitleTranslation: string;
+    tooltipTitleFallback: string;
     storeKey: keyof ReturnType<typeof store.getTodosCounts>;
   }
 > = {
   [TODOS_COUNT_TYPE.OVERDUE]: {
     Icon: components.IconCrossCircled,
-    tooltipTitle: 'Overdue',
+    tooltipTitleTranslation: 'views.App.AppNavbar.TodosCounts.overdue',
+    tooltipTitleFallback: 'Overdue',
     storeKey: 'unfinishedOverdueTodos',
   },
   [TODOS_COUNT_TYPE.TO_DO]: {
     Icon: components.IconCircle,
-    tooltipTitle: 'To be done',
+    tooltipTitleTranslation: 'views.App.AppNavbar.TodosCounts.todo',
+    tooltipTitleFallback: 'To be done',
     storeKey: 'unfinishedFutureTodos',
   },
   [TODOS_COUNT_TYPE.DONE]: {
     Icon: components.IconCheckCircled,
-    tooltipTitle: 'Done',
+    tooltipTitleTranslation: 'views.App.AppNavbar.TodosCounts.done',
+    tooltipTitleFallback: 'Done',
     storeKey: 'finishedTodos',
   },
 };
--- src/views/App/elements/AppNavbar/elements/TodosCounts/TodosCounts.tsx
+++ src/views/App/elements/AppNavbar/elements/TodosCounts/TodosCounts.tsx
@@ -4,9 +4,12 @@ import * as components from 'components';
 import * as styles from './TodosCounts.styles.ts';
 import * as constants from './TodosCounts.constants.ts';
 
+import { useTranslation } from 'react-i18next';
 import { useTodosCountsData } from './hooks';
 
 export const TodosCounts: FC = () => {
+  const { t } = useTranslation();
+
   const { storeData } = useTodosCountsData();
 
   return (
@@ -14,7 +17,13 @@ export const TodosCounts: FC = () => {
       <div className={styles.CLASSNAMES.iconsContainer}>
         {Object.entries(constants.TODOS_COUNT_CONFIG).map(
           ([countType, config]) => (
-            <components.Tooltip key={countType} title={config.tooltipTitle}>
+            <components.Tooltip
+              key={countType}
+              title={t(
+                config.tooltipTitleTranslation,
+                config.tooltipTitleFallback,
+              )}
+            >
               <div className={styles.CLASSNAMES.countContainer}>
                 <config.Icon
                   className={
